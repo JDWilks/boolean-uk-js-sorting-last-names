9e1c77dd69aba339fa11c9c25c89c52f65a923ad
Jonathon Wilks <jonathondwilks@icloud.com>
2021-04-28 15:17:26 +0100

    first stage complete (before challenge)

--- script.js
+++ script.js
@@ -12,9 +12,18 @@ names.sort()
 
 // - Have an output that tells you what is the position of your name in the sorted array
 
-let last_element = names[names.length - 1];
+// let last_element = names[names.length - 1];
+
+names.indexOf("Jonathon")
 
 // - Have an output that Uppercase's all names in the array
 
 string = names.join(' ').toUpperCase();
 
+// Let's take it a step further and group each sorted name with its upper-cased one in a sub-array, which in its turn will go inside another array to group it all together. 
+
+// It should look like this in your output:
+
+// [  ["Surname A", "SURNAME A"], ["Surname B", "SURNAME B"] ...other names ]
+
+// Output the result to the console.
